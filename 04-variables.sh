3be8bee6990f41dd156a7e207afec05d175d471a
Raman <ramanji.usa@gmail.com>
2024-04-16 18:42:33 -0500

    vvvv

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#I am trying to make it better by giving extenrllly user details
+
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hello $PERSON2 how are you ? "
+echo "$PERSON2: Hi $PERSON1 , I am good . how are you ? "
+echo "$PERSON1: going good . how is going devops practice" 
+echo "$PERSON2: going good $PERSON1 "
