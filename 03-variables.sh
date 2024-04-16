03bfd790e7cdef2b3d6f8d0442a419fdc41fbb02
Raman <ramanji.usa@gmail.com>
2024-04-16 18:39:39 -0500

    vvvv

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#I am trying to add some variables for 02 program
+
+
+PERSON1=Koushik
+PERSON2=Aasritha
+
+
+echo "$PERSON1: Hello $PERSON2 how are you ? "
+echo "$PERSON2: Hi $PERSON1 , I am good . how are you ? "
+echo "$PERSON1: going good . how is going devops practice" 
+echo "$PERSON2: going good $PERSON1 "
